72cff5f9df726a30cd9a10d309cad7c1698182ed
Julio Castro <j.frias.2198@gmail.com>
2024-03-14 21:17:18 -0400

    swapLastName program done

---
+++ lab-programacion1/LAB1/6.SwapLastName/SwapLastName/Program.cs
@@ -0,0 +1,43 @@
+﻿/*6.Recibir
+el nombres y apellidos de dos usuarios e imprimir por pantalla los nombres con
+los apellidos intercambiados.*/
+using System;
+
+namespace SwapLastName
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            string? answer;
+            do
+            {
+                string nomber1, appellido1, nombre2, apellido2;
+                Console.WriteLine(" Intercambiador de apellidos");
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Introduzca Primera persona Nombre: ");
+                nomber1 = Console.ReadLine();
+                Console.WriteLine("Introduzca Primera persona apellido: ");
+                appellido1 = Console.ReadLine();
+                Console.WriteLine("Introduzca Segunda Persona nombre: ");
+                nombre2 = Console.ReadLine();
+                Console.WriteLine("Introduzca Segunda Persona nombre: ");
+                apellido2 = Console.ReadLine();
+
+                Console.WriteLine("\nLos alpellidos han sido cambiados....");
+                Console.WriteLine($"\nPrimera Persona ahora es: {nomber1} {apellido2}\nLa Segunda persona Ahora es:{nombre2} {appellido1}");
+
+                Console.Write("\n\nDesea tratar nuevamente...Y(si)/ N(para salir): ");
+                answer = Console.ReadLine();
+                answer.ToLower().Trim();
+                while (answer != "n" && answer != "y")
+                {
+                    Console.WriteLine("Y(si)/ N(para salir):");
+                    answer = Console.ReadLine();
+                    answer.ToLower().Trim();
+                }
+            } while (answer == "y");                           
+        }
+    }
+}
