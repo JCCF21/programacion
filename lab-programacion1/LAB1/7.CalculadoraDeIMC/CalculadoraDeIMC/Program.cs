91f0aafaca55d44974c537691b2ec27c60dddbee
Julio Castro <j.frias.2198@gmail.com>
2024-03-16 08:10:13 -0400

    update folders name

---
+++ lab-programacion1/LAB1/7.CalculadoraDeIMC/CalculadoraDeIMC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de Indice de Masa Corporal (IMC)");
+            Console.Write("Ingresa tu peso en kilogramos: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingresa tu estatura en metros: ");
+            double estatura = Convert.ToDouble(Console.ReadLine());
+
+           
+            IMCCalculator calculadora = new IMCCalculator(peso, estatura);
+
+           
+            double imc = calculadora.CalcularIMC();
+
+                       Console.WriteLine($"Tu IMC es: {imc:F2}");
+
+            Console.ReadLine();
+        }
+    }
+    class IMCCalculator
+    {
+        private double Peso { get; }
+        private double Estatura { get; }
+
+        public IMCCalculator(double peso, double estatura)
+        {
+            Peso = peso;
+            Estatura = estatura;
+        }
+
+        public double CalcularIMC()
+        {
+            return Peso / (Estatura * Estatura);
+        }
+    }
+}
---
+++ lab-programacion1/LAB1/8.Persona/Persona/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Persona
+{
+    class Program
+    {
+        static void Main()
+        {
+            Persona persona = new Persona();
+
+            Console.Write("Por favor, ingresa tu nombre: ");
+            persona.Nombre = Console.ReadLine();
+
+            Console.Write("Por favor, ingresa tus apellidos: ");
+            persona.Apellidos = Console.ReadLine();
+
+            Console.Write("Por favor, ingresa tu edad: ");
+            persona.Edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Por favor, ingresa tu altura en metros: ");
+            persona.Altura = Convert.ToDouble(Console.ReadLine());
+
+            persona.MostrarInformacion();
+        }
+        class Persona
+        {
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public int Edad { get; set; }
+            public double Altura { get; set; }
+
+            public void MostrarInformacion()
+            {
+                Console.WriteLine("\nInformación ingresada:");
+                Console.WriteLine("Nombre: " + Nombre);
+                Console.WriteLine("Apellidos: " + Apellidos);
+                Console.WriteLine("Edad: " + Edad);
+                Console.WriteLine("Altura: " + Altura + " m");
+            }
+        }
+    }
+}
