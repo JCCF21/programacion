7c4be6fa0590fa1a645db71bdc46d518d6651b99
Julio Castro <j.frias.2198@gmail.com>
2024-03-18 21:23:56 -0400

    Adding two new programs of Lab2 AreaFriguras and calculadora

---
+++ lab-programacion1/LAB2/AreaFiguras/LAB2/AreaFiguras/Program.cs
@@ -0,0 +1,154 @@
+﻿//2.Pedir
+//al usuario el tipo de figura geométrica (triángulo, cuadrado, círculo) y
+//calcular su área utilizando switch.
+
+using System;
+using System.Formats.Asn1;
+
+namespace Lab2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string? opcion;
+            do
+            {
+
+                opcion = Menu();
+                
+              
+                switch (opcion)
+                {
+                    case "1":
+                        do
+                        {
+                            Console.Clear();
+                            double lado;
+                            Console.WriteLine("Calculadora del Area de un Cuadrado");
+                            Console.WriteLine("=======================================");
+
+                            Console.WriteLine("Ingrese la medida de los lados: ");
+                            Console.Write("Lado: ");
+                            lado = Convert.ToDouble(Console.ReadLine());
+                            Cuadrado cuadrado = new(lado);
+                            Console.WriteLine($"El area del Cuadrado de Lado {lado}: es {cuadrado.Area():N2}");
+
+                            opcion = Continuar();
+                        } while (opcion == "s" && opcion != "n");
+                        break;
+                    case "2":
+                        do
+                        {
+                            Console.Clear();
+                            double baseTriangulo, altura;
+                            Console.WriteLine("Calculadora del Area de un Triangulo");
+                            Console.WriteLine("=======================================");
+
+                            Console.WriteLine("Ingrese los datos requeridos");
+
+                            Console.Write("Base del Triandulo: ");
+                            baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+                            Console.Write("Altura del Triangulo: ");
+                            altura = Convert.ToDouble(Console.ReadLine());
+
+                            Triangulo triangulo = new Triangulo(baseTriangulo, altura);
+                            Console.WriteLine($"El Area del Triangulo de Base: {triangulo.BaseTriangulo} y Altura: {triangulo.Altura} es: {triangulo.Area().ToString("#,#.##")}");
+
+                            opcion = Continuar();
+                        } while (opcion == "s" && opcion != "n");
+                        break;
+                    case "3":
+                        do
+                        {
+                            Console.Clear();
+                            double radio;
+                            Console.WriteLine("Calculadora del Area de un Circulo");
+                            Console.WriteLine("=======================================");
+
+                            Console.WriteLine("Ingrese la medida del Radio: ");
+                            Console.Write("Radio: ");
+                            radio = Convert.ToDouble(Console.ReadLine());
+                            Circulo circulo = new(radio);
+                            Console.WriteLine($"El area del Circulo de Radio: {circulo.Radio} es {circulo.Area():N2}");
+
+                            opcion = Continuar();
+                        } while (opcion == "s" && opcion != "n");
+                        break;
+                    case "4":
+
+                        break;
+                    default:
+                        Console.WriteLine($"{Menu()} No es una opcion valida");
+                        break;
+                     
+                }
+            } while ( opcion != "4");
+
+        }
+        public static string Menu()
+        {
+            Console.Clear();
+            string? opcion;
+            Console.WriteLine("\t\tCalculadora de Area");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("Por favor, Elija la figura del area a calcular");
+            Console.WriteLine(" 1 - Cuadrado | 2 - Triangulo| 3 - Circulo| 4 - Salir  ");
+            Console.Write("opcion: ");
+            opcion = Console.ReadLine();
+
+            return opcion;
+
+        }
+        public static string Continuar()
+        {
+            string? opcion;
+            Console.WriteLine("\nDesea Continuar? S - Si  | N - No : ");
+            opcion = Console.ReadLine();
+
+            if (opcion != null)
+            {
+                opcion.ToLower().Trim();
+
+                while (opcion != "s" && opcion != "n")
+                {
+                    Console.Write("Opcion Invalidad...Trate Nuevamente:");
+                    opcion = Console.ReadLine();
+                }
+            }
+            return opcion;
+
+        }
+    }
+
+    class Cuadrado(double lado) : Figura
+    {
+        public double Lado { get; set; } = lado;
+        //formula lado * lado
+        public override double Area() => lado * lado;
+    }
+
+    class Triangulo(double baseTriangulo, double altura) : Figura
+    {
+        public double BaseTriangulo { get; } = baseTriangulo;
+        public double Altura { get; } = altura;
+        // Formula base * altura / 2
+        public override double Area() => (BaseTriangulo * Altura) / 2;
+
+    }
+    class Circulo(double radio) : Figura
+    {
+        public double Radio { get; } = radio;
+        //formula pi * r ^2
+        public override double Area() => Math.PI * (radio * radio);
+
+    }
+    public abstract class Figura()
+    {
+        public abstract double Area();
+
+    }
+}
+
---
+++ lab-programacion1/LAB2/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,117 @@
+﻿//1.Implementar
+//una calculadora que realice las operaciones básicas (+, -, *, /) con dos
+//números introducidos por el usuario. Utilizar switch para seleccionar
+//la operación.
+using System;
+using System.Security.Cryptography;
+
+namespace Asignacion2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? opcion;
+            Calculadora calculadora = new Calculadora();
+            do
+            {
+                
+                Console.WriteLine("\t\tCaluladora");
+                Console.WriteLine("**************************");
+
+                Console.WriteLine("Elige la operacion deseada: ");
+                Console.WriteLine("1- Sumar | 2- Restar | 3- Multiplicar | 4 - Dividir      |5- Salir");
+                Console.Write("Opcion: ");
+                opcion = Console.ReadLine();
+                
+               if(opcion != "5")
+               { 
+                Console.Write("Ingrese Primer Numero: ");
+                calculadora.NumA = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Ingrese Segundo Numero: ");
+                calculadora.NumB = Convert.ToDouble(Console.ReadLine());
+               }
+
+                if (opcion == "4" && calculadora.NumB == 0)
+                {
+                    while (calculadora.NumB == 0)
+                    {
+                        Console.WriteLine("Error Critico....");
+                        Console.WriteLine("El Segundo numbero No puede ser Cero...");
+                        calculadora.NumB = Convert.ToDouble(Console.ReadLine());
+                    }
+                }
+
+           
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine($"El Resulto de la Suma es: {calculadora.Suma(calculadora.NumA, calculadora.NumB).ToString("###,###,###.##")}");
+
+                        break;
+                    case "2":
+                        Console.WriteLine($"El Resultado de la Resta ES: {calculadora.Restar(calculadora.NumA, calculadora.NumB)}");
+                        break;
+
+                    case "3":
+                        Console.WriteLine($"El Resultado de la Multiplicacion ES: {calculadora.Multiplicar(calculadora.NumA, calculadora.NumB)}");
+                        break;
+                    case "4":
+                        Console.WriteLine($"El Resultado de la Division ES: {calculadora.Dividir(calculadora.NumA, calculadora.NumB)}");
+                        break;
+                    case "5":  break;
+                    default:
+                        Console.WriteLine("Opcion incorrecta Vulva a tratar...");
+                        break;
+                }
+                if(opcion != "5")
+                Console.WriteLine("Desea continuar...|S - Si  | N - No");
+                opcionInvalida:
+                opcion = Console.ReadLine();
+                if(opcion != null)
+                    opcion.ToLower().Trim();
+                switch(opcion)
+                {
+                    case"s":
+                    Console.Clear(); 
+                    break;
+                    case "n":
+                    break;
+                    default: Console.WriteLine($"{opcion} es una opcion invalida");
+                    goto opcionInvalida;
+                    
+                }
+            }while((opcion !="5" ||opcion == "s") && opcion != "n");
+           
+        }
+    }
+    public class Calculadora
+    {
+        private double _numA, _numB;
+
+        public double NumA { get; set; }
+        public double NumB { get; set; }
+
+        public double Suma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Restar(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+        public double Dividir(double a, double b)
+        {
+            return a / b;
+        }
+    }
+
+}
+
+
+
