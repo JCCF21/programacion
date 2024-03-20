0df92016ec6543b3c4006996f670813ab01c331e
Julio Castro <j.frias.2198@gmail.com>
2024-03-19 20:22:27 -0400

    4th assigned program done

---
+++ lab-programacion1/LAB2/4-Nota/Nota/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LAB2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? opcion;
+            do
+            {
+                Console.Clear();
+            
+                Console.WriteLine("\t<Verificador de Calificaciones>");
+                Console.WriteLine("================================================");
+                Console.WriteLine("Introduzca su calificacion:");
+                uint nota = ValidEntry();
+                if (nota >= 70)
+                {
+                    Console.WriteLine("Felicitaciones has Aprobado!! ");
+                }
+                else
+                {
+                    Console.WriteLine($"Lo siento has Reprobado... {nota} No es sufiente para aprobar... ");
+                }
+                Console.WriteLine("\n\nDeseas Verificar Otra Calificacion...|S - Si || Cualquier tecla para salir...");
+
+                opcion = Console.ReadLine();
+                if(opcion != null)
+                    opcion.ToLower().Trim();
+
+            }while (opcion == "s");
+
+        }
+        public static uint ValidEntry()
+        {
+            bool entry = false;
+            uint num = 0;
+            do
+            {
+                try
+                {
+                    num = Convert.ToUInt16(Console.ReadLine());
+                    entry = true;
+                } catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\nPor favor introduzca una Calificacion Valida: ");
+                }
+            }while(!entry);
+           return num;
+        }
+    }
+}
