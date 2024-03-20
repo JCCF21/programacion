3bd7d47f4f168f8fe6a02d40f209503ea7f64d7b
Julio Castro <j.frias.2198@gmail.com>
2024-03-19 20:24:02 -0400

    3th program completed

---
+++ lab-programacion1/LAB2/3-IfParOImpar/IfParOImpar/Program.cs
@@ -0,0 +1,55 @@
+﻿//3.Utilizar if para
+//verificar si un número introducido por el usuario es par o impar.
+using System;
+
+namespace LAB2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string? opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\t¿ Par O Impar ? ");
+                Console.WriteLine("================================");
+                Console.WriteLine("Introduzca Un numero: ");
+                int numero=0;
+                bool valid = false;
+                do
+                {
+                    try
+                    {
+                        numero = Convert.ToInt32(Console.ReadLine());
+                        valid = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($" Solo se abmiten numeros");
+                    }
+                    
+                }while (!valid);
+
+
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine($"El numero {numero.ToString("#,#")} es Par!");
+                }
+                else
+                {
+                    Console.WriteLine($"El numero {numero.ToString("#,#")} es Impar! ");
+                }
+                Console.WriteLine("\n\nPluse R para reinciar o culquier tecla para Salir....");
+                opcion = Console.ReadLine();
+                if(opcion != null)
+                opcion.Trim().ToLower();
+               
+            } while (opcion == "r") ;
+        }
+
+    }
+    
+
+}
